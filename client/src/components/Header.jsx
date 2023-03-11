e84ab9bb550c436f0b9cc0ce71d520dc90248d5b
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-03-12 00:00:51 +0100

    remove useless imports

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser, reset } from '../features/auth/authSlice';
 import logo from '../assets/logo.png';
