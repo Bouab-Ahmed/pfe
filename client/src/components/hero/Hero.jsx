f20385c7c9eb9eb4cb819b28af1f27cfd25c70ba
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-03-15 00:43:18 +0100

    hero section added

---
+++ client/src/components/hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import hero from '../../assets/hero-devices.png';
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col md:flex-row items-center justify-between h-[91vh] mx-10'>
+      <div className='flex flex-col items-start justify-start h-[60%] w-[55%]'>
+        <h1 className='text-6xl font-bold text-primary font-DINRoundPro leading-[70px] w-3/4'>
+          Hi, we're Knowledge hub.
+        </h1>
+        <div className='my-4'>
+          <p className='text-3xl text-textColor tracking-wide my-2'>
+            The world's most-loved social storytelling platform
+          </p>
+          <p className='text-2xl text-textColorLight tracking-wide my-4'>
+            Wattpad connects a global community of 90 million readers and
+            writers through the power of story.
+          </p>
+        </div>
+        <div className='flex gap-4'>
+          <button className='bg-primary text-white font-bold py-2 px-8 rounded-md'>
+            start writing
+          </button>
+          <button className='bg-primary text-white font-bold py-2 px-8 rounded-md'>
+            start reading
+          </button>
+        </div>
+      </div>
+      <div className='w-[45%]'>
+        <img src={hero} alt='hero' />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
