6cb7f3299db36ecf04aab0e4b2f96b3c611bda69
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-03-27 16:59:42 +0100

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,32 @@ import Feeds from './pages/Feeds';
 import Home from './pages/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register.jsx';
+import Otp from './pages/Otp';
+import { useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUser } from './features/auth/authSlice';
 function App() {
+  const location = useLocation();
+  const fullUrl = location.pathname;
+  console.log(fullUrl);
+
+  const user = useSelector(getUser);
+  console.log(user);
+
   return (
-    <div className='bg-[url(./assets/heder-bg.png)] bg-no-repeat bg-[right_top_3.7rem]'>
+    <div
+      className={`${
+        location.pathname === '/'
+          ? 'bg-[url(./assets/heder-bg.png)] bg-no-repeat bg-[right_top_3.7rem]'
+          : ''
+      }`}>
       <div className='container mx-auto'>
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path={'/auth/login'} element={<Login />} />
           <Route path={'/auth/register'} element={<Register />} />
+          <Route path={'/auth/otp'} element={<Otp />} />
           <Route path={'/feeds'} element={<Feeds />} />
         </Routes>
       </div>
