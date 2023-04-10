19646482f2e728c186c88c5d28e5d7b75e957c7c
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-10 17:51:11 +0100

    apply the protected route

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,34 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/navbar/Header';
-import Feeds from './pages/Feeds';
-import Home from './pages/Home';
+// import Feeds from './pages/Feeds';
+// import Home from './pages/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register.jsx';
 import Otp from './pages/Otp';
 import { useLocation } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { getUser } from './features/auth/authSlice';
+import ProtectedRoute from './components/protectedRoute/ProtectedRoute';
 function App() {
   const location = useLocation();
   const user = useSelector(getUser);
-  console.log(user);
 
   return (
     <div
       className={`${
         location.pathname === '/'
-          ? 'bg-[url(./assets/heder-bg.png)] bg-no-repeat bg-[right_top_3.7rem]'
+          ? 'bg-[url(./assets/heder-bg.png)] bg-no-repeat bg-[right_top_4.1rem]'
           : ''
       }`}>
-      <div className='container mx-auto'>
+      <div className='container1 mx-auto'>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<ProtectedRoute user={user} />} />
           <Route path={'/auth/login'} element={<Login />} />
           <Route path={'/auth/register'} element={<Register />} />
           <Route path={'/auth/otp'} element={<Otp />} />
-          <Route path={'/feeds'} element={<Feeds />} />
+          {/* <Route path={'/feeds'} element={<Feeds />} /> */}
         </Routes>
       </div>
       <ToastContainer />
