fa270d74a8aef32a73dae68007a74449df324f37
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-14 02:46:41 +0100

    updates

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -4,33 +4,33 @@ const API_URL = 'http://localhost:5000';
 
 // register user
 
-export const register = async (userData) => {
-  const response = await axios.post(API_URL + '/auth/register', userData);
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
-  }
-  return response.data;
+export const register = async(userData) => {
+    const response = await axios.post(API_URL + '/auth/register', userData);
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+    }
+    return response.data;
 };
 
 // login user
 
-export const login = async (userData) => {
-  const response = await axios.post(API_URL + '/auth/login', userData);
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
-  }
-  return response.data;
+export const login = async(userData) => {
+    const response = await axios.post(API_URL + '/auth/login', userData);
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+    }
+    return response.data;
 };
 
 export const logout = () => {
-  localStorage.removeItem('user');
+    localStorage.removeItem('user');
 };
 
 
 const authService = {
-  register,
-  logout,
-  login,
+    register,
+    logout,
+    login,
 };
 
 export default authService;
