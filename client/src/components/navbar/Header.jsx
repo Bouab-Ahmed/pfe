5fdb3e5cb53d18db2c6519cf8a24d79fe95092e4
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-28 07:31:01 +0100

    update navbar style

--- client/src/components/navbar/Header.jsx
+++ client/src/components/navbar/Header.jsx
@@ -1,15 +1,15 @@
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 // import logo from '../../assets/logo.png';
-import NavList from './NavList';
-import { useState, useEffect } from 'react';
+import NavList from "./NavList";
+import { useState, useEffect } from "react";
 import {
   Navbar,
   MobileNav,
   Button,
   IconButton,
-} from '@material-tailwind/react';
-import ProfileMenu from '../profileMenu/ProfileMenu';
+} from "@material-tailwind/react";
+import ProfileMenu from "../profileMenu/ProfileMenu";
 
 function Header() {
   const navigate = useNavigate();
@@ -18,78 +18,80 @@ function Header() {
 
   useEffect(() => {
     window.addEventListener(
-      'resize',
+      "resize",
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
 
   return (
-    <Navbar className='py-2 px-4 lg:px-8 lg:py-2 rounded-none shadow-sm min-w-full'>
-      <div className='flex items-center justify-between text-blue-gray-900'>
+    <Navbar className="py-2 px-4 lg:px-8 lg:py-2 rounded-none shadow-sm min-w-full">
+      <div className="flex items-center justify-between text-blue-gray-900 container1 mx-auto">
         <div
-          className='text-3xl text-primary font-DINRoundPro cursor-pointer'
-          onClick={() => navigate('/')}>
+          className="text-3xl text-primary font-DINRoundPro cursor-pointer"
+          onClick={() => navigate("/")}
+        >
           Logo
         </div>
-        <div className='flex items-center justify-between gap-3'>
-          <div className='hidden lg:block'>
+        <div className="flex items-center justify-between gap-3">
+          <div className="hidden lg:block">
             <NavList />
           </div>
           {user ? (
-            <div className='mx-4 bg-transparent my-0 py-0'>
+            <div className="mx-4 bg-transparent my-0 py-0">
               <ProfileMenu user={user} />
             </div>
           ) : (
             <Button
-              size='sm'
-              className='hidden lg:inline-block bg-gradient-to-r to-primary from-yellow-900 px-6'
-              onClick={() => navigate('/auth/login')}>
+              size="md"
+              className="hidden lg:inline-block bg-primary text-sm py-2 px-4 rounded-md"
+              onClick={() => navigate("/auth/login")}
+            >
               <span>Login</span>
             </Button>
           )}
         </div>
         <IconButton
-          variant='text'
-          className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
           ripple={false}
-          onClick={() => setOpenNav(!openNav)}>
+          onClick={() => setOpenNav(!openNav)}
+        >
           {openNav ? (
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              className='h-6 w-6'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              strokeWidth={2}>
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M6 18L18 6M6 6l12 12'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
               />
             </svg>
           ) : (
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth={2}>
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M4 6h16M4 12h16M4 18h16'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
           )}
         </IconButton>
       </div>
       <MobileNav open={openNav}>
-        <div className='container mx-auto'>
+        <div className="container mx-auto">
           <NavList />
-          <Button
-            size='sm'
-            fullWidth
-            className='mb-2 bg-gradient-to-r to-[#F9533E] from-yellow-900'>
+          <Button size="sm" fullWidth className="mb-2 bg-primary">
             <span>Login</span>
           </Button>
         </div>
