8385c13564bad52b50fbc77d037ff1770e1a8592
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-29 22:16:40 +0100

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Header from './components/navbar/Header';
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/navbar/Header";
 // import Feeds from './pages/Feeds';
 // import Home from './pages/Home';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register.jsx';
-import Otp from './pages/Otp';
-import { useSelector } from 'react-redux';
-import { getUser } from './features/auth/authSlice';
-import ProtectedRoute from './components/protectedRoute/ProtectedRoute';
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register.jsx";
+import Otp from "./pages/Otp";
+import { useSelector } from "react-redux";
+import { getUser } from "./features/auth/authSlice";
+import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
 function App() {
-  // get user from redux store
-  const user = useSelector(getUser);
-  console.log(user);
+  const user = localStorage.getItem("user");
   return (
     <div>
-      <div className='mx-auto'>
+      <div className="mx-auto">
         <Header />
         <Routes>
-          <Route path='/' element={<ProtectedRoute user={user} />} />
-          <Route path={'/auth/login'} element={<Login />} />
-          <Route path={'/auth/register'} element={<Register />} />
-          <Route path={'/auth/otp'} element={<Otp />} />
+          <Route path="/" element={<ProtectedRoute user={user} />} />
+          <Route path={"/auth/login"} element={<Login />} />
+          <Route path={"/auth/register"} element={<Register />} />
+          <Route path={"/auth/otp"} element={<Otp />} />
           {/* <Route path={'/feeds'} element={<Feeds />} /> */}
         </Routes>
       </div>
--- client/src/pages/Feeds.jsx
+++ client/src/pages/Feeds.jsx
@@ -5,7 +5,7 @@ import { getUser } from "../features/auth/authSlice";
 // get user from local storage
 
 const Feeds = () => {
-  const user = useSelector(getUser);
+  const user = localStorage.getItem("user");
   return (
     <div>
       <h1>Feeds</h1>
