56bd4ba46c72095d9adc77ec4657beb06fdd1da6
calem48 <messaoud.messaoud92@gmail.com>
2023-04-29 12:28:42 +0100

    start new branch

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+//     console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+const connectDB = (url) => {
+  mongoose.connect(url);
 };
 
 module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const { errHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const port = process.env.PORT || 5000;
-
-connectDB();
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
@@ -15,28 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-
 // routes
-app.use('/posts', require('./routes/postRoutes'));
-app.use('/auth', require('./routes/userRoutes'));
-
-// Serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-//     )
-//   );
-// } else {
-//   app.get('/', (req, res) => res.send('Please set to production'));
-// }
+app.use("/posts", require("./routes/postRoutes"));
+app.use("/auth", require("./routes/userRoutes"));
 
 app.use(errHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  await connectDB(process.env.MONGO_URI);
+  app.listen(port, () => console.log(`server connect at port ${port}`));
+};
+
+start();
