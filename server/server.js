ea5e8aa144c6169493a8e1aa0d67458bca9dd3b7
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-29 15:04:21 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const { errHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -15,27 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-
 // routes
-app.use('/posts', require('./routes/postRoutes'));
-app.use('/auth', require('./routes/userRoutes'));
-
-// Serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-//     )
-//   );
-// } else {
-//   app.get('/', (req, res) => res.send('Please set to production'));
-// }
+app.use("/posts", require("./routes/postRoutes"));
+app.use("/auth", require("./routes/userRoutes"));
 
 app.use(errHandler);
 
