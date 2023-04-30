eac508a949df9ee39ad9cf5f3833c091b3c87460
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-30 09:18:55 +0100

    merge updates

--- client/src/pages/Feeds.jsx
+++ client/src/pages/Feeds.jsx
@@ -1,11 +1,9 @@
-import React, { useRef } from "react";
-import { useSelector } from "react-redux";
-import { getUser } from "../features/auth/authSlice";
+import React from "react";
 
 // get user from local storage
 
 const Feeds = () => {
-  const user = useSelector(getUser);
+  const user = localStorage.getItem("user");
   return (
     <div>
       <h1>Feeds</h1>
--- server/server.js
+++ server/server.js
@@ -14,8 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+app.options("*", cors());
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
