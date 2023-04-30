67ccaebf1cfbe705abc231dcf3350ef7eb82ce36
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-30 09:06:00 +0100

    remove useless code

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/navbar/Header";
-// import Feeds from './pages/Feeds';
-// import Home from './pages/Home';
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register.jsx";
 import Otp from "./pages/Otp";
-import { useSelector } from "react-redux";
-import { getUser } from "./features/auth/authSlice";
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
 function App() {
   const user = localStorage.getItem("user");
