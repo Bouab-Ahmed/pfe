52e7cbb57f3e585daf41555751053bd4caee808d
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-01 17:21:29 +0100

    add auth middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const {
   registerUser,
   loginUser,
-  sendOtp,
   verifyEmail,
   logout,
 } = require("../controllers/authControllers");
+const { auth } = require("../middleware/authMiddleware");
 
 router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
-router.route("/otp").post(sendOtp);
-router.route("/verifyEmail").post(verifyEmail);
+router.route("/verifyEmail").post(auth, verifyEmail);
 router.route("/logout").get(logout);
 
 module.exports = router;
