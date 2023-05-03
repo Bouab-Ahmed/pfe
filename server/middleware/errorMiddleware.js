41f08ddf6ae981b8fc12189c8165916823df68ce
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-03 12:26:12 +0100

    note: work on this file

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,37 +1,33 @@
-// const errHandler = (err, req, res, next) => {
-//   const statusCode = res.statusCode ? res.statusCode : 500;
-
-//   res.status(statusCode);
-
-//   res.json({
-//     message: err.message,
-//     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-//   });
-
-//   next();
-// };
-
-// module.exports = {
-//   errHandler,
-// };
-
-const { StatusCodes } = require("http-status-codes");
-
 const middlewareErrorHandler = (err, req, res, next) => {
-  const customError = {
-    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || `there something happened try later`,
-  };
+  const statusCode = res.statusCode ? res.statusCode : 500;
 
-  if (err.code === 11000) {
-    customError.statusCode = StatusCodes.BAD_REQUEST;
-    customError.message = `this  ${Object.keys(
-      err.keyValue
-    )} has been registered before`;
-  }
+  res.status(statusCode);
 
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
-  return res.status(customError.statusCode).json({ msg: customError.message });
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+
+  next();
 };
+
+// const { StatusCodes } = require("http-status-codes");
+
+// const middlewareErrorHandler = (err, req, res, next) => {
+//   const customError = {
+//     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+//     message: err.message || `there something happened try later`,
+//   };
+
+//   if (err.code === 11000) {
+//     customError.statusCode = StatusCodes.BAD_REQUEST;
+//     customError.message = `this  ${Object.keys(
+//       err.keyValue
+//     )} has been registered before`;
+//   }
+
+//   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+//   return res.status(customError.statusCode).json({ msg: customError.message });
+// };
 
 module.exports = middlewareErrorHandler;
