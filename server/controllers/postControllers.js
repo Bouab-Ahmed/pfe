1d974060c00f8003594e695b20dae6261ca44ac2
calem48 <messaoud.messaoud92@gmail.com>
2023-05-06 22:30:17 +0100

    edit all the post controller logique

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -4,21 +4,22 @@ const User = require("../models/userModel");
 
 const createNewPost = async (req, res) => {
   const post = await Post.create({ ...req.body, user: req.user.userId });
-  res.status(200).json(post);
+  res.status(200).json({ post });
 };
 
 const getAllPosts = async (req, res) => {
   const posts = await Post.find({});
-  res.status(200).json({ posts });
+  res.status(200).json(posts);
 };
 
 const getSinglePost = async (req, res) => {
   console.log(req.params.id);
   const post = await Post.findOne({ _id: req.params.id });
+
   if (!post) {
     throw new CustomError.NotFoundError("not found any product ");
   }
-  res.status(200).json(post);
+  res.status(200).json({ post, commentLength: post.comments.length });
 };
 
 const updatePost = async (req, res) => {
