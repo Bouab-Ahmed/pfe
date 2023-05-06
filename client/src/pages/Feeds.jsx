87723fa32a0fdfcfe66b37cd066219e076b9f052
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-06 13:57:45 +0100

    update feed section

--- client/src/pages/Feeds.jsx
+++ client/src/pages/Feeds.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import Posts from "../components/posts/Posts";
+import Aside from "../components/aside/Aside";
 
 // get user from local storage
 
 const Feeds = () => {
   const user = localStorage.getItem("user");
   return (
-    <div className="">
-      <h1>Feeds</h1>
-      {user && (
-        <>
-          <h2>welcome {user.name}</h2>
-          <img src={user.cardId} alt="" />
-          <img src={user.profilePic} alt="" />
-        </>
-      )}
+    <div className="container1 mx-auto flex justify-between my-2">
+      <div className="flex-[2]">
+        <Posts />
+      </div>
+      <div className="flex-1">
+        <Aside />
+      </div>
     </div>
   );
 };
