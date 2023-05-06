03e172b0d5e6c7b1474a7c64b0cfb6bed92cdab7
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-06 13:58:28 +0100

    updates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,19 @@
-const withMT = require('@material-tailwind/react/utils/withMT');
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                DINRoundPro: ['Montserrat', 'sans-serif'],
-                sourceSansPro: ['Source Sans Pro', 'sourceSansPro'],
-            },
-            colors: {
-                primary: '#45995D',
-                textColor: '#222222',
-                textColorLight: '#8C887E',
-            },
-        },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        DINRoundPro: ["Montserrat", "sans-serif"],
+        sourceSansPro: ["Source Sans Pro", "sourceSansPro"],
+      },
+      colors: {
+        primary: "#45995D",
+        textColor: "#222222",
+        textColorLight: "#8C887E",
+      },
     },
-    plugins: [],
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
 });
