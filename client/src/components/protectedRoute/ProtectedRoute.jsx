793bbaf4548f94f6d70842bd86d915f3155be302
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-06 15:13:04 +0100

    updates

--- client/src/components/protectedRoute/ProtectedRoute.jsx
+++ client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import Feeds from "../../pages/Feeds";
 import Home from "../../pages/Home";
 
 const ProtectedRoute = ({ user }) => {
-  console.log(user);
   if (user) {
     return <Feeds />;
   } else {
