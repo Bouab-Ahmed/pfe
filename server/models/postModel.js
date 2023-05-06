7a6ae7f575dff8354d23a641ee84b04612546a22
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-06 15:12:51 +0100

    post infos

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,20 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const postSChema = mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    text: {
-      type: String,
-      required: [true, 'Please add some text'],
-    },
+    // user: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   required: true,
+    //   ref: 'User',
+    // },
+    // user id, title, content, image, likes(like, dislike), comments, createdAt, tags
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = mongoose.model('Post', postSChema);
+module.exports = mongoose.model("Post", postSChema);
