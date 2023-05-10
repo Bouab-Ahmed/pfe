b42e5c96e7d4573983dc62c5ad6ab7aa4b0ffd96
calem48 <messaoud.messaoud92@gmail.com>
2023-05-10 05:49:59 +0100

    change all logique od commentController

--- server/controllers/commentControllers.js
+++ server/controllers/commentControllers.js
@@ -1,34 +1,54 @@
 const { NotFoundError } = require("../errors");
-const Post = require("../models/postModel");
+const Comments = require("../models/commentModel");
 
 const createNewComment = async (req, res) => {
-  //   req.user.userId;
-  const { comment } = req.body;
+  req.body.user = req.user.userId;
+  req.body.post = req.body.postId;
+  const comment = await Comments.create({ ...req.body });
 
-  const post = await Post.findById({ _id: req.body.postId });
+  const commentWithInfoUser = await comment.populate({
+    path: "user",
+    select: "name profilePic -_id",
+  });
 
-  if (!post) {
-    throw new NotFoundError("not found any post ");
-  }
+  res.status(201).json({ commentWithInfoUser });
+};
 
-  const addPost = await post.addComment(req.user.userId, comment);
-
-  res.status(201).json(addPost);
+const createAllommentofPost = async (req, res) => {
+  // const comment = await Comments.find({ _id: req.body.postId });
+  // if (!comment) {
+  //   throw new NotFoundError("not found any comment ");
+  // }
+  // res.status(200).json({ comment });
 };
 
 const updateComment = async (req, res) => {
-  res.status(200).json("update");
+  const comment = await Comments.findByIdAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true }
+  );
+
+  if (!comment) {
+    throw new NotFoundError("not found any comment ");
+  }
+
+  res.status(200).json(comment);
 };
 
 const deleteComment = async (req, res) => {
-  const comment = await Post.findOne({ _id: req.body.postId });
+  const comment = await Comments.findOne({ _id: req.params.id });
   if (!comment) {
-    throw new NotFoundError("not found any post ");
+    throw new NotFoundError("not found any comment ");
   }
+  await comment.remove();
 
-  const post = await comment.removeComment(req.params.id);
-
-  res.status(200).json({ msg: "deleted comment" });
+  res.status(200).json({ msg: "delete comment success" });
 };
 
-module.exports = { createNewComment, updateComment, deleteComment };
+module.exports = {
+  createNewComment,
+  updateComment,
+  deleteComment,
+  createAllommentofPost,
+};
