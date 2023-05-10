85ecda80fefd66b8454aaa94dd12afa0c8e50390
calem48 <messaoud.messaoud92@gmail.com>
2023-05-10 12:22:00 +0100

    add new route reply

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -6,10 +6,15 @@ const {
   createNewComment,
   updateComment,
   deleteComment,
-  createAllommentofPost,
+  getAllCommentofPost,
+  replyComment,
 } = require("../controllers/commentControllers");
 
-router.route("/").post(auth, createNewComment).get(auth, createAllommentofPost);
+router
+  .route("/")
+  .post(auth, createNewComment)
+  .get(auth, getAllCommentofPost)
+  .patch(auth, replyComment);
 router.route("/:id").patch(auth, updateComment).delete(auth, deleteComment);
 
 module.exports = router;
