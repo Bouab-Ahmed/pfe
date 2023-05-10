cbc7a8b6fc75c3b186eb6624b7d0050efdfca13b
calem48 <messaoud.messaoud92@gmail.com>
2023-05-10 05:53:39 +0100

    edit at the getSinglePost

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,9 +1,10 @@
 const { NotFoundError } = require("../errors");
 const Post = require("../models/postModel");
-const User = require("../models/userModel");
+// const User = require("../models/userModel");
 
 const createNewPost = async (req, res) => {
   const post = await Post.create({ ...req.body, user: req.user.userId });
+
   res.status(200).json({ post });
 };
 
@@ -13,12 +14,23 @@ const getAllPosts = async (req, res) => {
 };
 
 const getSinglePost = async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
+  const post = await Post.findOne({ _id: req.params.id })
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+        select: "name profilePic -_id",
+      },
+    })
+    .populate({
+      path: "user",
+      select: "name profilePic -_id",
+    });
 
   if (!post) {
     throw new NotFoundError("not found any product ");
   }
-  res.status(200).json({ post, commentLength: post.comments.length });
+  res.status(200).json({ post });
 };
 
 const updatePost = async (req, res) => {
