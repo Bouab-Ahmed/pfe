13c2068f62fae4cf7c9ac652d128ccc138a29377
calem48 <messaoud.messaoud92@gmail.com>
2023-05-11 20:30:41 +0100

    update and add some info

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -22,14 +22,18 @@ const userSchema = mongoose.Schema(
       enum: ["writer", "reader", "admin"],
       default: "reader",
     },
-    fellows: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "User",
-    },
-    fellowers: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "User",
-    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
     cardId: {
       type: String,
       require: [true, "you must to provide cardId"],
@@ -47,6 +51,12 @@ const userSchema = mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    tags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag",
+      },
+    ],
   },
   {
     timestamps: true,
@@ -59,12 +69,6 @@ userSchema.pre("save", async function (next) {
   }
   this.password = await bcrypt.hash(this.password, 10);
 });
-
-// userSchema.methods.generateToken = async function () {
-//   return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRES_IN,
-//   });
-// };
 
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
