812e25cfaf131b871a0aa1f1d90e0c5c0745c3d4
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-12 01:04:59 +0100

    add post slice

--- client/src/features/posts/postsService.js
+++ client/src/features/posts/postsService.js
@@ -1,18 +1,42 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:5000';
+const API_URL = "http://localhost:5000";
 
-
-const createPost = async(post, token) => {
-    const config = {
-        headers: { Authorization: `Bearer ${token}` },
-    };
-    const request = await axios.post(API_URL + '/newPost', post, config);
-    return request.data;
-}
-
-const postService = {
-    createPost
+export const createPost = async (post, token) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  const request = await axios.post(API_URL + "/newPost", post, config);
+  return request.data;
 };
 
-export default postService;
+export const getPosts = async (thunkAPI) => {
+  const res = await fetch(API_URL + "/posts", {
+    method: "GET",
+  });
+
+  if (!res.ok) {
+    return thunkAPI.rejectWithValue(await res.json());
+  }
+  return await res.json();
+};
+
+export const getSinglePost = async (id, thunkAPI) => {
+  const res = await fetch(API_URL + "/posts/" + id, {
+    method: "GET",
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    return thunkAPI.rejectWithValue(await res.json());
+  }
+  return await res.json();
+};
+
+const postsService = {
+  createPost,
+  getPosts,
+  getSinglePost,
+};
+
+export default postsService;
--- client/src/features/posts/postsSlice.js
+++ client/src/features/posts/postsSlice.js
@@ -1,64 +1,86 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import postService from './postsService';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import postsService from "./postsService";
 
 const initialState = {
-    posts: [],
-    isError: false,
-    isLoading: false,
-    isSuccess: false,
-    message: '',
+  posts: [],
+  singlePost: null,
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
 };
 
 // create post
-
 export const createPost = createAsyncThunk(
-    'post/createPost',
-    async(post, thunkAPI) => {
-        try {
-            const token = thunkAPI.getState().auth.user;
-            return await postService.createPost(post, token);
-        } catch (error) {
-            const message =
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString();
-            return thunkAPI.rejectWithValue(message);
-        }
-    }
+  "post/createPost",
+  async (post, thunkAPI) => {
+    return postsService.createPost(post, thunkAPI);
+  }
 );
 
+// get posts
 
+export const getPosts = createAsyncThunk(
+  "posts/getPosts",
+  async (_, thunkAPI) => {
+    return postsService.getPosts(thunkAPI);
+  }
+);
+
+export const getSinglePost = createAsyncThunk(
+  "post/getSinglePost",
+  async (id, thunkAPI) => {
+    return postsService.getSinglePost(id, thunkAPI);
+  }
+);
 
 export const postsSlice = createSlice({
-    name: 'post',
-    initialState,
-    reducers: {
-        reset: (state) => initialState,
-    },
-    extraReducers: {
-        addCase: (builder) => {
-            builder.addCase(createPost.pending, (state) => {
-                state.isLoading = true;
-                state.isError = false;
-                state.isSuccess = false;
-                state.message = '';
-            });
-            builder.addCase(createPost.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.isError = false;
-                state.isSuccess = true;
-                state.message.push(action.payload);
-            });
-            builder.addCase(createPost.rejected, (state, action) => {
-                state.isLoading = false;
-                state.isError = true;
-                state.isSuccess = false;
-                state.message.push(action.payload);
-            });
-        }
-    },
+  name: "post",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createPost.pending, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    });
+    builder.addCase(createPost.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = true;
+      state.message.push(action.payload.msg);
+      state.posts = [...action.payload.posts];
+    });
+    builder.addCase(createPost.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.isSuccess = false;
+      state.message.push(action.payload.msg);
+    });
+    builder.addCase(getSinglePost.pending, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    });
+    builder.addCase(getSinglePost.fulfilled, (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = true;
+      state.message = action.payload.msg;
+      state.singlePost = action.payload.post;
+    });
+    builder.addCase(getSinglePost.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.isSuccess = false;
+      // state.message = action.payload.msg;
+    });
+  },
 });
 
 export const { reset } = postsSlice.actions;
