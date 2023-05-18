35a291257f1f13483132271bccc55dcfc4a76090
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-18 23:33:33 +0100

    add reducers to app

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import postsReducer from '../features/posts/postsSlice';
+import userReducer from '../features/users/userSlice';
+import commentReducer from '../features/comments/commentSlice';
+import tagReducer from '../features/tags/tagSlice.js';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     post: postsReducer,
+    user: userReducer,
+    comment: commentReducer,
+    tag: tagReducer,
   },
 });
