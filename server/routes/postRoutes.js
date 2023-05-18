b3f252d5944b76920d5030297de228c729a1a00c
calem48 <messaoud.messaoud92@gmail.com>
2023-05-19 00:33:23 +0100

    added new route for like and dislike

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -6,15 +6,20 @@ const {
   createNewPost,
   updatePost,
   deletePost,
+  like,
+  dislike
 } = require("../controllers/postControllers");
 const { auth } = require("../middleware/authMiddleware");
 
 router.route("/").get(auth, getAllPosts).post(auth, createNewPost);
+router.route("/like").put(auth, like)
+router.route("/dislike").put(auth, dislike)
 
 router
   .route("/:id")
   .get(auth, getSinglePost)
   .put(auth, updatePost)
+
   .delete(auth, deletePost);
 
 module.exports = router;
