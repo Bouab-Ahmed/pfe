e58aef6ddeb42d92c0fe5d908cdc87da4a241b8d
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-18 23:33:56 +0100

    update aside (make it dynamic)

--- client/src/components/aside/Aside.jsx
+++ client/src/components/aside/Aside.jsx
@@ -1,137 +1,82 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTags } from "../../features/posts/postsSlice";
+import { useNavigate } from "react-router-dom";
+import { getAllusers } from "../../features/users/userSlice";
 
-import authorPic from "../../assets/default.jpg";
+const Aside = ({user}) => {
 
-const Aside = () => {
-  const recommended = [
-    "Fiction",
-    "Non-fiction",
-    "Mystery",
-    "Thriller",
-    "Romance",
-    "Science Fiction",
-    "Fantasy",
-    "Horror",
-    "Historical Fiction",
-    "Biography",
-    "Autobiography",
-    "Memoir",
-    "Self-help",
-    "Business",
-    "Politics",
-    "History",
-    "Travel",
-    "Cooking",
-    "Art",
-    "Poetry",
-    "Drama",
-    "Religion",
-    "Philosophy",
-    "Psychology",
-    "Sociology",
-    "Education",
-    "Science",
-    "Sports",
-    "Children's",
-    "Young Adult",
-  ];
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
 
-  let authors = [
-    {
-      id: 1,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 2,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 3,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 4,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 5,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 6,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 7,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 8,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 9,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 10,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 11,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 12,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 13,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-    {
-      id: 14,
-      name: "Candice Wu",
-      fellow: "Follow",
-    },
-  ];
+  const { tags, isLoading, isSuccess, isError } = useSelector(
+    (state) => state.post
+  );
 
-  const [follow, setFollow] = useState(authors);
-  const onFollow = (fellow, id) => {
-    const newAuthors = authors.map((author) => {
-      if (author.id === id) {
-        author.fellow = fellow === "Follow" ? "Following" : "Follow";
-      }
-      return author;
-    });
-    setFollow(newAuthors);
-  };
+  const { users, isUserLoading, isUserSuccess, isUserError } = useSelector((state) => state.user);
 
+
+
+  // const [follow, setFollow] = useState(authors);
+  // const onFollow = (fellow, id) => {
+  //   const newAuthors = authors.map((author) => {
+  //     if (author.id === id) {
+  //       author.fellow = fellow === "Follow" ? "Following" : "Follow";
+  //     }
+  //     return author;
+  //   });
+  //   setFollow(newAuthors);
+  // };
+
+    useEffect(() => {
+      dispatch(getTags());
+      dispatch(getAllusers());
+      // if (!user) {
+      //   dispatch(getAllusers());
+      // }
+      // eslint-disable-next-line
+    }, []);
+
+    console.log(users)
   return (
-    <div className="flex flex-col items-center justify-center sticky top-14 z-10">
+    <div className="flex flex-col justify-center sticky top-14 z-10">
+      {user && (
+        <div className="flex flex-col items-center justify-center gap-1 w-3/4 text-center">
+          <div className="w-24 h-w-24 rounded-full">
+            <img
+              src={`http://localhost:5000${user?.profilePic}`}
+              alt="author"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <h3
+            className="my-1 text-lg cursor-pointer"
+            onClick={() => navigate(`/me/${user._id}`)}
+          >
+            {user?.name}
+          </h3>
+          <div className="flex flex-row gap-2 ">
+            <span>{user?.follower.length} followers</span> •{" "}
+            <span>{user?.following.length} following</span>
+          </div>
+          <p className="text-[#292929] opacity-75 text-sm">
+            Creative Investor. Sharing my inspiring journey in...
+          </p>
+          <button className="w-full rounded-full px-4 py-1 border border-primary text-white bg-primary">
+            Follow
+          </button>
+        </div>
+      )}
       <div className="mx-1 my-2">
         <h2 className="text-2xl font-bold my-2">Recommended Topics</h2>
         <div className="flex flex-wrap gap-2">
-          {recommended.slice(0, 10).map((topic, i) => (
+          {tags?.slice(0, 10)?.map((tag) => (
             <span
               className="p-2 bg-gray-100 rounded-full text-sm text-[#292929] cursor-pointer"
-              key={i}
+              key={tag._id}
             >
               {" "}
-              {topic}{" "}
+              {tag.name}{" "}
             </span>
           ))}
         </div>
@@ -139,35 +84,40 @@ const Aside = () => {
           <a href="/topics">Discover more</a>
         </h3>
       </div>
-      <div className="mx-1 my-2">
-        <h2 className="text-2xl font-bold my-2">who to fellow</h2>
-        <div className="flex flex-wrap gap-2">
-          {follow.slice(0, 5).map((author, i) => (
-            <div className="flex flex-row my-1 items-center" key={i}>
-              <img
-                src={authorPic}
-                alt="author"
-                className="h-8 w-8 rounded-full self-start"
-              />
-              <div className="flex flex-col mx-2">
-                <span className="text-lg font-bold"> {author.name} </span>
-                <p className="text-[#292929] opacity-75 text-sm">
-                  Creative Investor. Sharing my inspiring journey in...
-                </p>
-              </div>
-              <button
-                className="rounded-full px-8 py-2 border border-black"
-                onClick={() => onFollow(author.fellow, author.id)}
-              >
-                {author.fellow}
-              </button>
-            </div>
-          ))}
-          <h3 className="my-1 text-primary">
-            <a href="/topics">See more Suggestions</a>
-          </h3>
+      {user ? (
+        <h2 className="text-2xl font-bold my-2">more from this user</h2>
+      ) : (
+        <div className="mx-1 my-2">
+          <h2 className="text-2xl font-bold my-2">recommended authors</h2>
+          <div className="flex flex-wrap gap-2">
+            {users &&
+              users.slice(0, 5).map((author, i) => (
+                <div className="flex flex-row my-1 items-center" key={i}>
+                  <img
+                    src={`http://localhost:5000${author.profilePic}`}
+                    alt="author"
+                    className="h-8 w-8 rounded-full self-start"
+                  />
+                  <div className="flex flex-col mx-2">
+                    <span className="text-lg font-bold"> {author.name} </span>
+                    <p className="text-[#292929] opacity-75 text-sm">
+                      Creative Investor. Sharing my inspiring journey in...
+                    </p>
+                  </div>
+                  <button
+                    className="rounded-full px-8 py-2 border border-black"
+                    // onClick={() => onFollow(author.fellow, author.id)}
+                  >
+                    follow
+                  </button>
+                </div>
+              ))}
+            <h3 className="my-1 text-primary">
+              <a href="/topics">See more Suggestions</a>
+            </h3>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
