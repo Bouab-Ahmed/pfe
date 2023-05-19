e8b621b7afd7d3808a99877288c3dc95750b8d59
calem48 <messaoud.messaoud92@gmail.com>
2023-05-19 17:15:49 +0100

    add logique of like, dislike in controller of post

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -6,12 +6,16 @@ const { uploadProductImageLocal } = require("./uploadsController");
 
 const createNewPost = async (req, res) => {
   const pathImg = await uploadProductImageLocal(req);
-  
-  const post = await Post.create({ ...req.body, user: req.user.userId, image: pathImg });
 
-  await post.decrease(req.body.idTag)
+  const post = await Post.create({
+    ...req.body,
+    user: req.user.userId,
+    image: pathImg,
+  });
 
-  res.status(200).json({ post }); 
+  await post.decrease(req.body.idTag);
+
+  res.status(200).json({ post });
 };
 
 const getAllPosts = async (req, res) => {
@@ -20,13 +24,15 @@ const getAllPosts = async (req, res) => {
       // { following: { $in: req.user.following } },
       { tags: { $in: req.user.tags } },
     ],
-  }).populate({
-    path: "tags",
-    select: "name _id",
-  }).populate({
-    path: "user",
-    select: "name profilePic _id following follower createdAt",
-  });
+  })
+    .populate({
+      path: "tags",
+      select: "name _id",
+    })
+    .populate({
+      path: "user",
+      select: "name profilePic _id following follower createdAt",
+    });
 
   if (!posts.length) {
     throw new NotFoundError("not found any post ");
@@ -95,15 +101,53 @@ const deletePost = async (req, res) => {
   res.status(200).json({ post });
 };
 
-const like = async (req,res)=>{
-  const user = await Post.findOne({ _id: req.params.id })
-  res.status(200).json("like");
-}
+const like = async (req, res) => {
+  const post = await Post.findById({ _id: req.params.id });
 
-const dislike = async (req,res)=>{
-  res.status(200).json("dislike");
+  if (!post) {
+    throw new NotFoundError("this post not found");
+  }
 
-}
+  const isLiked = await Post.findOne({
+    _id: req.params.id,
+    "like.user": req.user.userId,
+  });
+
+  if (!isLiked) {
+    post.dislike.pop({ user: req.user.userId });
+    post.like.push({ user: req.user.userId });
+    await post.save();
+  } else {
+    post.like.pop({ user: req.user.userId });
+    await post.save();
+  }
+
+  res.status(200).json({ likes: post.like.length });
+};
+
+const dislike = async (req, res) => {
+  const post = await Post.findById({ _id: req.params.id });
+
+  if (!post) {
+    throw new NotFoundError("this post not found");
+  }
+
+  const isDislike = await Post.findOne({
+    _id: req.params.id,
+    "dislike.user": req.user.userId,
+  });
+
+  if (!isDislike) {
+    post.like.pop({ user: req.user.userId });
+    post.dislike.push({ user: req.user.userId });
+    await post.save();
+  } else {
+    post.dislike.pop({ user: req.user.userId });
+    await post.save();
+  }
+
+  res.status(200).json({ dislikes: post.dislike.length });
+};
 
 module.exports = {
   getAllPosts,
@@ -112,5 +156,5 @@ module.exports = {
   updatePost,
   deletePost,
   like,
-  dislike
+  dislike,
 };
