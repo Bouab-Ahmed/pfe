a18528f6ac252deba1e1dcd6f4a8f36287193151
calem48 <messaoud.messaoud92@gmail.com>
2023-05-19 17:13:56 +0100

    edit route of like and dislike

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,19 +7,18 @@ const {
   updatePost,
   deletePost,
   like,
-  dislike
+  dislike,
 } = require("../controllers/postControllers");
 const { auth } = require("../middleware/authMiddleware");
 
 router.route("/").get(auth, getAllPosts).post(auth, createNewPost);
-router.route("/like").put(auth, like)
-router.route("/dislike").put(auth, dislike)
+router.route("/:id/like").put(auth, like);
+router.route("/:id/dislike/").put(auth, dislike);
 
 router
   .route("/:id")
   .get(auth, getSinglePost)
   .put(auth, updatePost)
-
   .delete(auth, deletePost);
 
 module.exports = router;
