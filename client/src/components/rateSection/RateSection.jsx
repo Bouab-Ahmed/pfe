ab250162073c395e71cfc864b6061ba76e42f535
calem48 <messaoud.messaoud92@gmail.com>
2023-05-21 23:53:20 +0100

    edit rateSection like and dislike

--- client/src/components/rateSection/RateSection.jsx
+++ client/src/components/rateSection/RateSection.jsx
@@ -2,82 +2,45 @@ import React, { useEffect, useState } from "react";
 import { BiDownvote, BiUpvote } from "react-icons/bi";
 import { sendDislike, sendLike } from "../../features/rate/rateSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { getSinglePost } from "../../features/posts/postsSlice";
 
 const RateSection = ({ postId, user, like, dislike }) => {
-  const [postactiveBtn, setPostActiveBtn] = useState("none");
   const dispatch = useDispatch();
-  const { likes, dislikes, isRateSuccess } = useSelector((state) => state.rate);
+  const { likes, dislikes, isRateSuccess, isDisliked, isliked } = useSelector(
+    (state) => state.rate
+  );
 
-	const [postRate, setPostRate] = useState({
-		like: [],
-		dislike: [],
-	})
+  const [upVote, setUpvote] = useState(likes);
+  const [downVote, setDownVote] = useState(dislikes);
 
-	useEffect(() => {
-		setPostRate({
-			like: like?.length,
-			dislike: dislike?.length,
-		})
-	}, [like, dislike])
-
-
-
-  const handlePostLikes = (clicked) => {
-    if (!user) {
-      return;
+  useEffect(() => {
+    if (isRateSuccess) {
+      dispatch(getSinglePost(postId));
+      setUpvote(likes);
+      setDownVote(dislikes);
     }
-    if (clicked === "upvote") {
-      dispatch(sendLike(postId));
-
-      // if (postactiveBtn === "upvote") {
-      //   setPostActiveBtn("none");
-      // } else if (postactiveBtn === "none") {
-      //   setPostActiveBtn("upvote");
-      // } else {
-      //   setPostActiveBtn("upvote");
-      // }
-    } else if (clicked === "downvote") {
-      dispatch(sendDislike(postId));
-      // if (postactiveBtn === "downvote") {
-      //   setPostActiveBtn("none");
-      // } else if (postactiveBtn === "upvote") {
-      //   setPostActiveBtn("downvote");
-      // } else if (postactiveBtn === "none") {
-      //   setPostActiveBtn("downvote");
-      // }
-    }
-  };
-
-	useEffect(() => {
-		if (isRateSuccess) {
-			setPostRate({
-				like: likes,
-				dislike: dislikes,
-			})
-		}
-	}, [isRateSuccess, likes, dislikes]);
-
+  }, [likes, dislikes]);
 
   return (
     <div className="flex items-center gap-1">
       <BiUpvote
-        className={`text-2xl stroke-[0.5] ${
-          postactiveBtn === "upvote"
+        className={`text-2xl stroke-[0.5] disable ${
+          like?.some((id) => id.user === user?._id)
             ? "bg-transparent fill-primary stroke-primary"
             : "bg-transparent"
-        } `}
-        onClick={() => handlePostLikes("upvote")}
+        }  `}
+        onClick={() => dispatch(sendLike(postId))}
       />
-      <span className="text-xl">{postRate.like}</span>
+      <span className="text-xl">{upVote || like?.length}</span>
       <BiDownvote
         className={`text-2xl stroke-[0.5] ${
-          postactiveBtn === "downvote"
+          dislike?.some((id) => id.user === user?._id)
             ? "fill-primary stroke-primary"
             : "bg-transparent"
         } `}
-        onClick={() => handlePostLikes("downvote")}
+        onClick={() => dispatch(sendDislike(postId))}
       />
-      <span className="text-xl">{postRate.dislike}</span>
+      <span className="text-xl">{downVote || dislike?.length}</span>
     </div>
   );
 };
