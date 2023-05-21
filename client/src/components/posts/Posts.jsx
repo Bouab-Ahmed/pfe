a2fbc3e3baf3d3a23bba5fb85b9d23403ad9c29b
calem48 <messaoud.messaoud92@gmail.com>
2023-05-21 23:53:50 +0100

    change the messge

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -3,7 +3,6 @@ import PostCard from "./PostCard";
 import { getPosts, reset } from "../../features/posts/postsSlice";
 import { useDispatch, useSelector } from "react-redux";
 
-
 const Posts = () => {
   const dispatch = useDispatch();
   const { posts, isPostLoading, isPostSuccess, isPostError } = useSelector(
@@ -19,17 +18,15 @@ const Posts = () => {
   }
 
   if (isPostError) {
-    return <h1>Something went wrong</h1>;
+    return <h1>you don't have any post you follow</h1>;
   }
 
-
-  console.log(posts)
+  console.log(posts);
 
   return (
     <div className="flex flex-col items-center">
-      { isPostSuccess && posts.map((post) => (
-        <PostCard key={post.id} {...post} />
-      ))}
+      {isPostSuccess &&
+        posts.map((post) => <PostCard key={post.id} {...post} />)}
       <div>
         <button className="text-[#222222] font-bold py-2 px-4 rounded-lg bg-gray-300 ">
           follow more categories to see more posts
