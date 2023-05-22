62bb5ddf6c71996e4a23afba8d5ef0b40acfd6a3
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-22 18:30:19 +0100

    update posts section

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from "react";
 import PostCard from "./PostCard";
-import { getPosts, reset } from "../../features/posts/postsSlice";
+import { getPosts } from "../../features/posts/postsSlice";
 import { useDispatch, useSelector } from "react-redux";
-
 
 const Posts = () => {
   const dispatch = useDispatch();
+  const path = window.location.pathname.split("/")[1];
   const { posts, isPostLoading, isPostSuccess, isPostError } = useSelector(
     (state) => state.post
   );
 
   useEffect(() => {
     dispatch(getPosts());
+    // eslint-disable-next-line
   }, []);
 
   if (isPostLoading) {
@@ -22,19 +23,21 @@ const Posts = () => {
     return <h1>Something went wrong</h1>;
   }
 
-
-  console.log(posts)
+  console.log(posts);
 
   return (
     <div className="flex flex-col items-center">
-      { isPostSuccess && posts.map((post) => (
-        <PostCard key={post.id} {...post} />
-      ))}
-      <div>
+      {isPostSuccess &&
+        posts.map((post) => <PostCard key={post.id} {...post} />)}
+      {
+        path !=="profile" && (
+          <div>
         <button className="text-[#222222] font-bold py-2 px-4 rounded-lg bg-gray-300 ">
           follow more categories to see more posts
         </button>
       </div>
+        )
+      }
     </div>
   );
 };
