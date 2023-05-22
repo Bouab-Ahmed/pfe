d79a21135a12c0fc6255c20be9514fcd48af9d33
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-22 18:33:25 +0100

    complete landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
 import Trending from "../components/trending/Trending";
+import Features from "../components/features/Features";
+import Team from "../components/team/Team";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <div className="">
       <Hero />
       <Trending />
+      <Features />
+      <Team />
+      <Footer />
     </div>
   );
 };
