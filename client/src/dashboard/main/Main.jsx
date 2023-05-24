3046d7c60bbf8e169754d49f39bb26e755fed247
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-24 23:51:48 +0100

    add cards to main page

--- client/src/dashboard/main/Main.jsx
+++ client/src/dashboard/main/Main.jsx
@@ -1,15 +1,33 @@
-import React from "react";
-import ManagePosts from "../managePosts/ManagePosts";
-import ManageCategories from "../manageCategories/ManageCategories";
-import ManageUsers from "../manageUsers/ManageUsers";
-import Settings from "../settings/Settings";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllusers } from "../../features/users/userSlice";
+import { getRandomPosts } from "../../features/posts/postsSlice";
+import { getTags } from "../../features/tags/tagSlice";
+import DashCard from "../components/DashCard";
 
-const Main = ({page}) => {
-	return (
-		<div >
-			main
-		</div>
-	);
+const Main = () => {
+  const dispatch = useDispatch();
+
+  const { isUserSuccess, users } = useSelector((state) => state.user);
+  const { isPostSuccess, posts } = useSelector((state) => state.post);
+  const { isTagSuccess, tags } = useSelector((state) => state.tag);
+
+  useEffect(() => {
+    dispatch(getAllusers());
+    dispatch(getRandomPosts());
+    dispatch(getTags());
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-between my-2 w-full">
+      <div className=" p-4 flex w-full justify-evenly gap-4">
+        {isUserSuccess && <DashCard title={"Users"} value={users.length} />}
+        {isPostSuccess && <DashCard title={"Posts"} value={posts.length} />}
+        {isTagSuccess && <DashCard title={"Tags"} value={tags.length} />}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
