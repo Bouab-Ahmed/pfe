56b6ffe3d7352e0ce1c593d473e259ced7989f82
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-25 02:51:35 +0100

    fix post model

--- client/src/dashboard/components/PostModel.jsx
+++ client/src/dashboard/components/PostModel.jsx
@@ -60,31 +60,31 @@ const PostModel = ({open, handleOpen,post}) => {
           </div>
           <div className="flex items-center gap-4">
             <button
-              class="inline-flex items-center px-4 py-2 text-red-600 text-sm border border-red-600 font-medium rounded-md"
+              className="inline-flex items-center px-4 py-2 text-red-600 text-sm border border-red-600 font-medium rounded-md"
               onClick={handleOpen}
             >
               Cancel
             </button>
             {post?.stauts === "published" ? (
-              <button class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 border-2 text-white text-sm font-medium rounded-md">
+              <button className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 border-2 text-white text-sm font-medium rounded-md">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 mr-2"
+                  className="h-5 w-5 mr-2"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                   />
                 </svg>
                 Delete
               </button>
             ) : (
-              <button class="inline-flex items-center px-4 py-2 bg-primary hover:bg-green-800 border-2 text-white text-sm font-medium rounded-md">
+              <button className="inline-flex items-center px-4 py-2 bg-primary hover:bg-green-800 border-2 text-white text-sm font-medium rounded-md">
                 <FcApproval className="text-xl mr-1" />
                 Approve
               </button>
