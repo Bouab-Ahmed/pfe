d38eeca6a9db219aa12cbffb73b55afb4c73fca1
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-25 02:51:59 +0100

    fix manage post page

--- client/src/dashboard/managePosts/ManagePosts.jsx
+++ client/src/dashboard/managePosts/ManagePosts.jsx
@@ -59,15 +59,7 @@ const ManagePosts = () => {
             title: "Title",
             dataIndex: "title",
             key: "title",
-            sorter: function (a, b) {
-              if (a.name < b.name) {
-                return -1;
-              }
-              if (a.name > b.name) {
-                return 1;
-              }
-              return 0;
-            },
+            sorter: (a, b) => a.localeCompare(b),
             width: "20%",
           },
           {
@@ -128,36 +120,36 @@ const ManagePosts = () => {
             dataIndex: "action",
             key: "action",
             width: "15%",
-            render: (_,row) => {
+            render: (_, row) => {
               return (
                 <div className="flex flex-row justify-evenly">
                   <button
-                    class="inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md"
+                    className="inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md"
                     onClick={() => getPostModel(row.key)}
                   >
                     <AiOutlineEye className="text-xl mr-1" />
                     view
                   </button>
                   {row.status === "published" ? (
-                    <button class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                    <button className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-5 w-5 mr-2"
+                        className="h-5 w-5 mr-2"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                         />
                       </svg>
                       Delete
                     </button>
                   ) : (
-                    <button class="inline-flex items-center px-4 py-2 bg-primary hover:bg-green-800 text-white text-sm font-medium rounded-md">
+                    <button className="inline-flex items-center px-4 py-2 bg-primary hover:bg-green-800 text-white text-sm font-medium rounded-md">
                       <FcApproval className="text-xl mr-1" />
                       Approve
                     </button>
