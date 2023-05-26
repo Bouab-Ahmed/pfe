69813ce1d280648b2acef7abaeac2c6f99a28f65
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-26 17:06:37 +0100

    update protected route

--- client/src/components/protectedRoute/ProtectedRoute.jsx
+++ client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Feeds from "../../pages/Feeds";
 import Home from "../../pages/Home";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const user = useSelector((state) => state.auth.user);
   if (user) {
-    return <Feeds />;
+    if (user.tags.length > 0) {
+      return <Feeds />;
+    } else {
+      return <Navigate to="/topics" />;
+    }
   } else {
     return <Home />;
   }
