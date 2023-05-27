96c16081478fb0d69fdcebda9873356156014f75
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-05-27 11:34:49 +0100

    fix navlist

--- client/src/components/navbar/NavList.jsx
+++ client/src/components/navbar/NavList.jsx
@@ -1,31 +1,17 @@
-import { Typography } from '@material-tailwind/react';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import navItems from '../../shared/navitems';
-import DropList from './DropList';
+import React from "react";
+import navItems from "../../shared/navitems";
+import { useNavigate } from "react-router-dom";
 
 const NavList = () => {
+  const navigate = useNavigate()
   return (
-    <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row items-center lg:gap-2'>
-      {navItems.map((item, i) =>
-        item.name === 'Browse' ? (
-          <DropList item={item.name} categories={item.categories} key={i} />
-        ) : item.name === 'Language' ? (
-          <DropList item={item.name} categories={item.languages} key={i} />
-        ) : (
-          <Typography
-            as='li'
-            variant='small'
-            color='blue-gray'
-            className='font-semibold text-base'
-            key={i}>
-            <Link to={item.path} className='mb-1 flex items-center'>
-              {item.name}
-            </Link>
-          </Typography>
-        )
-      )}
-    </ul>
+    <div className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row items-center lg:gap-6">
+      {navItems.map((item) => (
+        <span key={item.path} onClick={()=>navigate(item.path)}>
+          {item.name}
+        </span>
+      ))}
+    </div>
   );
 };
 
