37a135c917aa0026f793dd414dc73e6d64c532dd
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-06-02 03:27:27 +0100

    add responsive design

--- client/src/components/trending/Trending.jsx
+++ client/src/components/trending/Trending.jsx
@@ -26,12 +26,12 @@ const Trending = () => {
 
 
   return (
-    <div className={`${path === "/"?"bg-[#F1FAF7]":"bg-white"} py-10`}>
+    <div className={`${path === "/" ? "bg-[#F1FAF7]" : "bg-white"} py-10 px-4 sm:px-0`}>
       <div className="flex justify-between items-center container1 mx-auto my-4">
-        <h1 className="text-4xl font-bold">Trending on platform</h1>
+        <h1 className="text-3xl md:text-4xl font-bold">Trending on platform</h1>
         {path === "/" && (
           <button
-            className="bg-primary text-white font-bold py-2 px-8 rounded-sm"
+            className="bg-primary text-white font-bold py-2 px-4 sm:px-8 rounded-sm text-base"
             onClick={() => navigate("/trending")}
           >
             see more
@@ -39,7 +39,7 @@ const Trending = () => {
         )}
       </div>
 
-      <div className="grid grid-cols-3 gap-3 container1 mx-auto ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 container1 mx-auto ">
         {isPostSuccess &&
           posts.map((post) => <PostCard key={post.id} {...post} />)}
       </div>
