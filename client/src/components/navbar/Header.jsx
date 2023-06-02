426848fa23f0909ced7f6fe109da193df6fcffa0
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-06-02 03:25:57 +0100

    update navbar

--- client/src/components/navbar/Header.jsx
+++ client/src/components/navbar/Header.jsx
@@ -79,7 +79,11 @@ function Header() {
             ) : (
               <div className="flex items-center gap-4">
                 <NavList />
-                {user && <span className="py-2 px-3 bg-primary text-white rounded-3xl">Become a Writer</span>}
+                {user && (
+                  <span className="py-2 px-3 bg-primary text-white rounded-3xl">
+                    Become a Writer
+                  </span>
+                )}
               </div>
             )}
           </div>
@@ -97,48 +101,55 @@ function Header() {
             </Button>
           )}
         </div>
-        <IconButton
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          )}
-        </IconButton>
+        {!user && (
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        )}
       </div>
       <MobileNav open={openNav}>
         <div className="container mx-auto">
           <NavList />
-          <Button size="sm" fullWidth className="mb-2 bg-primary">
+          <Button
+            size="sm"
+            fullWidth
+            className="mb-2 bg-primary"
+            onClick={() => navigate("/auth/login")}
+          >
             <span>Login</span>
           </Button>
         </div>
--- client/src/components/navbar/NavList.jsx
+++ client/src/components/navbar/NavList.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const NavList = () => {
   const navigate = useNavigate()
   return (
-    <div className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row items-center lg:gap-6">
+    <div className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row items-center lg:gap-6 text-gray-900 mb-1">
       {navItems.map((item) => (
         <span key={item.path} onClick={()=>navigate(item.path)}>
           {item.name}
