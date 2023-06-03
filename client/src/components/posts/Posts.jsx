ef33f7496283e115c975401c9d2b3d0eaf7a9bd6
calem48 <messaoud.messaoud92@gmail.com>
2023-06-03 23:39:10 +0100

    add userEffect

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,17 +4,29 @@ import { getPosts } from "../../features/posts/postsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { resetComment } from "../../features/comments/commentSlice";
 import { useNavigate } from "react-router-dom";
+import { getMe } from "../../features/users/userSlice";
 
 const Posts = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const path = window.location.pathname.split("/")[1];
   const { posts, isPostLoading, isPostSuccess, isPostError } = useSelector(
     (state) => state.post
   );
+  const { isUserSuccessGetMe } = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch(getPosts());
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    dispatch(getPosts());
+    // eslint-disable-next-line
+  }, [isUserSuccessGetMe]);
+
+  useEffect(() => {
+    dispatch(getMe());
     // eslint-disable-next-line
   }, []);
 
@@ -38,15 +50,13 @@ const Posts = () => {
           </button>
         </div>
       )}
-      {path !== "profile" &&
-        posts.length > 5 &&
-        isPostSuccess &&(
-          <div>
-            <button className="text-[#222222] font-bold py-2 px-4 rounded-lg bg-gray-300 ">
-              follow more categories to see more posts
-            </button>
-          </div>
-        )}
+      {path !== "profile" && posts.length > 5 && isPostSuccess && (
+        <div>
+          <button className="text-[#222222] font-bold py-2 px-4 rounded-lg bg-gray-300 ">
+            follow more categories to see more posts
+          </button>
+        </div>
+      )}
     </div>
   );
 };
